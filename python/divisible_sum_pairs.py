b7196b8bc3ce9a1016eb1a7b070c4c8d006f90a0
v-t-9 <violeta.a.t.9@gmail.com>
2024-02-01 20:15:41 -0300

    3rd commit python and 1st commit sql

---
+++ python/divisible_sum_pairs.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    coord = []
+    res = 0
+   
+    for i in range(n):
+        for j in range(1, n):
+            if i < j:
+                coord.append((i, j))
+    
+    for i in coord:
+        if (ar[i[0]] + ar[i[1]]) % k == 0:
+            res +=1
+            #print(ar[i[0]], ar[i[1]])
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ python/sales_by_match.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    d = dict()
+    count = 0
+    for i in range(n):
+        d[ar[i]] = ar.count(ar[i])
+
+    for i in list(d.values()):
+        while i / 2 >= 1:
+            count += 1
+            i-=2
+    return count
+        
+        
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
